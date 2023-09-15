e877ad29c2ea6027e7bca9d155a6790c7de347bd
Nikolay Kolganov <nikolaykolganov7@gmail.com>
2023-09-15 17:53:48 +0300

    Add basic slices

---
+++ src/redux/slices/postsSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  posts: [],
+}
+
+export const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {
+    setPosts: (state, action) => {
+        state.posts = action.payload
+    },
+    editPosts: (state, action) => {
+        // edit post
+    },
+    getPosts: (state, action) => {
+        // return post by id
+    },
+    addPosts: (state, action) => {
+        // add new post by data
+    },
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { setPosts, editPosts, addPosts, getPosts } = postsSlice.actions
+
+export default postsSlice.reducer
