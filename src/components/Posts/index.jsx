bf456a89e01ca1c5ba076e5cb155123cb291a322
Nikolay Kolganov <nikolaykolganov7@gmail.com>
2023-09-15 22:45:04 +0300

    Create main page

---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {Post} from './components/Post'
+import * as SC from './styles'
+
+export const Posts = ({posts}) => (<>
+        <SC.Title>Свежие публикации</SC.Title>
+        <SC.Posts>
+            {posts.map((post) => <Post post={post} />)}
+        </SC.Posts>
+    </>
+)
