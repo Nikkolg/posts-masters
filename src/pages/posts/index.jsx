e0d591c96c5d839ed62afb38c4e2ffe4d47268bf
Nikolay Kolganov <nikolaykolganov7@gmail.com>
2023-09-17 13:49:33 +0300

    Integrate redux into app

--- src/pages/posts/index.jsx
+++ src/pages/posts/index.jsx
@@ -1,52 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Posts } from "../../components/Posts";
 import { Typo } from "../../components/Typo";
 import { Container } from "../../components/Container"
 
-export const INITIAL_POSTS = [
-    {
-        id: 1,
-        title: 'Post 1',
-        image: 'https://cdnn11.img.sputnik.by/img/07e6/0a/1b/1068422950_0:0:1200:676_1920x0_80_0_0_db203c19f1168a18ef2e6b80ef9c49eb.jpg',
-        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates voluptatum harum, molestias porro ab laudantium perferendis laboriosam nemo itaque eius tempore voluptas obcaecati vel minus quas minima. Voluptas, quod magni!'
-    },
-    {
-        id: 2,
-        title: 'Post 2',
-        image: 'https://cdnn11.img.sputnik.by/img/07e6/0a/1b/1068422950_0:0:1200:676_1920x0_80_0_0_db203c19f1168a18ef2e6b80ef9c49eb.jpg',
-        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates voluptatum harum, molestias porro ab laudantium perferendis laboriosam nemo itaque eius tempore voluptas obcaecati vel minus quas minima. Voluptas, quod magni!'
-    },
-    {
-        id: 3,
-        title: 'Post 3',
-        image: 'https://cdnn11.img.sputnik.by/img/07e6/0a/1b/1068422950_0:0:1200:676_1920x0_80_0_0_db203c19f1168a18ef2e6b80ef9c49eb.jpg',
-        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates voluptatum harum, molestias porro ab laudantium perferendis laboriosam nemo itaque eius tempore voluptas obcaecati vel minus quas minima. Voluptas, quod magni!'
-    },
-    {
-        id: 4,
-        title: 'Post 4',
-        image: 'https://cdnn11.img.sputnik.by/img/07e6/0a/1b/1068422950_0:0:1200:676_1920x0_80_0_0_db203c19f1168a18ef2e6b80ef9c49eb.jpg',
-        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates voluptatum harum, molestias porro ab laudantium perferendis laboriosam nemo itaque eius tempore voluptas obcaecati vel minus quas minima. Voluptas, quod magni!'
-    },
-    {
-        id: 5,
-        title: 'Post 5',
-        image: 'https://cdnn11.img.sputnik.by/img/07e6/0a/1b/1068422950_0:0:1200:676_1920x0_80_0_0_db203c19f1168a18ef2e6b80ef9c49eb.jpg',
-        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates voluptatum harum, molestias porro ab laudantium perferendis laboriosam nemo itaque eius tempore voluptas obcaecati vel minus quas minima. Voluptas, quod magni!'
-    },
-    {
-        id: 6,
-        title: 'Post 6',
-        image: 'https://cdnn11.img.sputnik.by/img/07e6/0a/1b/1068422950_0:0:1200:676_1920x0_80_0_0_db203c19f1168a18ef2e6b80ef9c49eb.jpg',
-        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates voluptatum harum, molestias porro ab laudantium perferendis laboriosam nemo itaque eius tempore voluptas obcaecati vel minus quas minima. Voluptas, quod magni!'
-    },
-]
-
-export const PostsPage = () => (
-    <>
-    <Container >
-        <Typo>Публикации</Typo>
-        <Posts posts={INITIAL_POSTS} />
-    </Container>
-    </>
-)
+export const PostsPage = () => {
+    const posts = useSelector((state) => state.posts.list)
+    return (
+        <Container >
+            <Typo>Публикации</Typo>
+            <Posts posts={posts} />
+        </Container>
+)}
--- src/redux/slices/postsSlice.js
+++ src/redux/slices/postsSlice.js
@@ -1,7 +1,57 @@
 import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
-  posts: [],
+    list: [
+        {
+            id: 8,
+            title: 'Post 8',
+            image: 'https://cdnn11.img.sputnik.by/img/07e6/0a/1b/1068422950_0:0:1200:676_1920x0_80_0_0_db203c19f1168a18ef2e6b80ef9c49eb.jpg',
+            text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates voluptatum harum, molestias porro ab laudantium perferendis laboriosam nemo itaque eius tempore voluptas obcaecati vel minus quas minima. Voluptas, quod magni!'
+        },
+        {
+            id: 7,
+            title: 'Post 7',
+            image: 'https://cdnn11.img.sputnik.by/img/07e6/0a/1b/1068422950_0:0:1200:676_1920x0_80_0_0_db203c19f1168a18ef2e6b80ef9c49eb.jpg',
+            text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates voluptatum harum, molestias porro ab laudantium perferendis laboriosam nemo itaque eius tempore voluptas obcaecati vel minus quas minima. Voluptas, quod magni!'
+        },
+        {
+            id: 6,
+            title: 'Post 6',
+            image: 'https://cdnn11.img.sputnik.by/img/07e6/0a/1b/1068422950_0:0:1200:676_1920x0_80_0_0_db203c19f1168a18ef2e6b80ef9c49eb.jpg',
+            text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates voluptatum harum, molestias porro ab laudantium perferendis laboriosam nemo itaque eius tempore voluptas obcaecati vel minus quas minima. Voluptas, quod magni!'
+        },
+        {
+            id: 5,
+            title: 'Post 5',
+            image: 'https://cdnn11.img.sputnik.by/img/07e6/0a/1b/1068422950_0:0:1200:676_1920x0_80_0_0_db203c19f1168a18ef2e6b80ef9c49eb.jpg',
+            text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates voluptatum harum, molestias porro ab laudantium perferendis laboriosam nemo itaque eius tempore voluptas obcaecati vel minus quas minima. Voluptas, quod magni!'
+        },
+        {
+            id: 4,
+            title: 'Post 4',
+            image: 'https://cdnn11.img.sputnik.by/img/07e6/0a/1b/1068422950_0:0:1200:676_1920x0_80_0_0_db203c19f1168a18ef2e6b80ef9c49eb.jpg',
+            text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates voluptatum harum, molestias porro ab laudantium perferendis laboriosam nemo itaque eius tempore voluptas obcaecati vel minus quas minima. Voluptas, quod magni!'
+        },
+        {
+            id: 3,
+            title: 'Post 3',
+            image: 'https://cdnn11.img.sputnik.by/img/07e6/0a/1b/1068422950_0:0:1200:676_1920x0_80_0_0_db203c19f1168a18ef2e6b80ef9c49eb.jpg',
+            text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates voluptatum harum, molestias porro ab laudantium perferendis laboriosam nemo itaque eius tempore voluptas obcaecati vel minus quas minima. Voluptas, quod magni!'
+        },{
+            id: 2,
+            title: 'Post 2',
+            image: 'https://cdnn11.img.sputnik.by/img/07e6/0a/1b/1068422950_0:0:1200:676_1920x0_80_0_0_db203c19f1168a18ef2e6b80ef9c49eb.jpg',
+            text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates voluptatum harum, molestias porro ab laudantium perferendis laboriosam nemo itaque eius tempore voluptas obcaecati vel minus quas minima. Voluptas, quod magni!'
+        },
+        {
+            id: 1,
+            title: 'Post 1',
+            image: 'https://cdnn11.img.sputnik.by/img/07e6/0a/1b/1068422950_0:0:1200:676_1920x0_80_0_0_db203c19f1168a18ef2e6b80ef9c49eb.jpg',
+            text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates voluptatum harum, molestias porro ab laudantium perferendis laboriosam nemo itaque eius tempore voluptas obcaecati vel minus quas minima. Voluptas, quod magni!'
+        },
+    ],
+    postForView: null,
+    freshPosts: null,
 }
 
 export const postsSlice = createSlice({
@@ -15,7 +65,10 @@ export const postsSlice = createSlice({
         // edit post
     },
     getPosts: (state, action) => {
-        // return post by id
+        state.postForView = state.list.find((item) => item.id === action.payload)
+    },
+    getFreshPosts: (state) => {
+        state.freshPosts = state.list.slice(0,4)
     },
     addPosts: (state, action) => {
         // add new post by data
@@ -23,7 +76,6 @@ export const postsSlice = createSlice({
   },
 })
 
-// Action creators are generated for each case reducer function
-export const { setPosts, editPosts, addPosts, getPosts } = postsSlice.actions
+export const { setPosts, editPosts, addPosts, getPosts, getFreshPosts } = postsSlice.actions
 
 export default postsSlice.reducer
